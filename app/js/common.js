5514593a2e2c8af393e5611579de41b2999032de
Serge Tiutin <aerofan@ukr.net>
2018-11-25 01:37:11 +0200

    update homepage markup. added calculator

--- app/js/common.js
+++ app/js/common.js
@@ -43,13 +43,91 @@ const borderInterval = {from: 0.1, to: 1};
 const graphicInterval = {from: 60, to: 0};
 const textInterval = {from: 0, to: -20};
 
-$(function() {
-	$(".meter > span").each(function() {
-		$(this)
-			.data("origWidth", $(this).width())
-			.width(0)
-			.animate({
-				width: $(this).data("origWidth")
-			}, 1200);
+
+/* Monthly calculator */
+
+$(document).ready( function () {
+	
+	var resultMonth, resultYear;
+	var calculator = $('.monthly-calculator');
+	var calculatorRange = calculator.find('.monthly-calculator-range');
+	var calculatorResultMonth = calculator.find('.monthly-calculator-result-month');
+	var calculatorResultYear = calculator.find('.monthly-calculator-result-year');
+	var amountVal = calculator.find('.monthly-calculator-amount-val');
+
+	$(window).on('DOMContentLoaded', function () {
+		amountVal.val( calculatorRange.val() );
+		resultMonth = calculatorRange.val() * 0.12;
+		resultYear = calculatorRange.val() * 1.25;
+		calculatorResultMonth.text( resultMonth.toFixed(0) + ' $' );
+		calculatorResultYear.text( resultYear.toFixed(0) + ' $' );
 	});
+	
+	calculatorRange.on('change', function () {
+		amountVal.val( calculatorRange.val() );
+		calculatorRange.val( amountVal.val() );
+		resultMonth = calculatorRange.val() * 0.12;
+		resultYear = calculatorRange.val() * 1.25;
+		calculatorResultMonth.text( resultMonth.toFixed(0) + ' $' );
+		calculatorResultYear.text( resultYear.toFixed(0) + ' $' );
+	});
+	
+	amountVal.on('change', function () {
+		amountVal.val( calculatorRange.val() );
+		calculatorRange.val( amountVal.val() );
+		resultMonth = calculatorRange.val() * 0.12;
+		resultYear = calculatorRange.val() * 1.25;
+		calculatorResultMonth.text( resultMonth.toFixed(0) + ' $' );
+		calculatorResultYear.text( resultYear.toFixed(0) + ' $' );
+	});
+
 });
+
+/* Daily calculator */
+
+$(document).ready( function () {
+	
+	var resultMonthD, resultDayD;
+	var dailyCalculator = $('.daily-calculator');
+	var dailyCalculatorRange = dailyCalculator.find('.daily-calculator-range');
+	var dailyCalculatorResultDay = dailyCalculator.find('.daily-calculator-result-day');
+	var dailyCalculatorResultMonth = dailyCalculator.find('.daily-calculator-result-month');
+	var dailyCalculatorAmountVal = dailyCalculator.find('.daily-calculator-amount-val');
+
+	$(window).on('DOMContentLoaded', function() {
+		dailyCalculatorAmountVal.val(dailyCalculatorRange.val());
+		resultDayD = (dailyCalculatorRange.val() * 0.12) / 31.5;
+		resultMonthD = dailyCalculatorRange.val() * 0.12;
+		dailyCalculatorResultDay.text(resultDayD.toFixed(2) + ' $');
+		dailyCalculatorResultMonth.text(resultMonthD.toFixed(0) + ' $');
+	});
+	
+	dailyCalculatorRange.on('change', function() {
+		dailyCalculatorAmountVal.val(dailyCalculatorRange.val());
+		dailyCalculatorRange.val(dailyCalculatorAmountVal.val());
+		resultDayD = (dailyCalculatorRange.val() * 0.12) / 31.5;
+		resultMonthD = dailyCalculatorRange.val() * 0.12;
+		dailyCalculatorResultDay.text(resultDayD.toFixed(2)  + ' $');
+		dailyCalculatorResultMonth.text(resultMonthD.toFixed(0) + ' $');
+	});
+	
+	dailyCalculatorAmountVal.on('change', function() {
+		dailyCalculatorAmountVal.val(dailyCalculatorRange.val());
+		dailyCalculatorRange.val(dailyCalculatorAmountVal.val());
+		resultDayD = (dailyCalculatorRange.val() * 0.12) / 31.5;
+		resultMonthD = dailyCalculatorRange.val() * 0.12;
+		dailyCalculatorResultDay.text(resultDayD.toFixed(2) + ' $');
+		dailyCalculatorResultMonth.text(resultMonthD.toFixed(0) + ' $');
+	});
+
+});
+
+function openMobileMenu() {
+	document.getElementById("mobile-menu").style.height = "100%";
+	$("#hamburger").css('display', 'none');
+}
+
+function closeMobileMenu() {
+	document.getElementById("mobile-menu").style.height = "0%";
+	$("#hamburger").css('display', 'flex');
+}
